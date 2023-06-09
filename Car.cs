92ce581902c3cf12b9c85526b753202ad3cb7bed
Radosław Smoroński <43452468+Johanez07@users.noreply.github.com>
2023-06-09 16:08:59 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMD
+{
+    internal class Car
+    {
+        private int posX = 0;
+        private int posY = 0;
+        private string carProject = "[o{}{}o]";
+
+        public Car() { }
+
+        public int GetPosX() { return this.posX; }
+        public int GetPosY() { return this.posY; }
+        public string GetCarProject()
+        {
+            return this.carProject;
+        }
+
+        public void GoLeft()
+        {
+            this.posX--;
+        }
+
+        public void GoRight()
+        {
+            this.posX++;
+        }
+
+        public void GoUp()
+        {
+            this.posY--;
+        }
+
+        public void GoDown()
+        {
+            this.posY++;
+        }
+
+    }
+}
