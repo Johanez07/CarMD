0a28b583cbb77fb50babc371fa276c635087540c
Radosław Smoroński <43452468+Johanez07@users.noreply.github.com>
2023-06-12 23:54:06 +0200

    Add files via upload

---
+++ DisplayCar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMD
+{
+    internal class DisplayCar
+    {
+        public DisplayCar() { }
+
+        public void Display(Car car)
+        {
+            int posX = car.GetPosX();
+            int posY = car.GetPosY();
+
+            bool engineStatus = car.GetEngineStatus();
+
+            string[] carDisplay = car.GetCarProject();
+
+
+            Console.WriteLine("CarMD v. 0.1 - 09/06/2023");
+            Console.WriteLine("Car Engine: " + engineStatus);
+
+
+            string lineX = "";
+
+            for (int y = 0; y < posY; y++)
+            {
+                Console.WriteLine();
+            }
+
+            for (int x = 0; x < posX; x++)
+            {
+                lineX = lineX + " ";
+            }
+
+            for (int i = 0; i < carDisplay.Length; i++)
+            {
+                Console.WriteLine(lineX + carDisplay[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CarMD;
+using System.Runtime.ConstrainedExecution;
+using System.Windows.Input;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Car myCar = new Car();
+        DisplayCar screen = new DisplayCar();
+
+        while(true) {
+            Console.Clear();
+
+            screen.Display(myCar);
+
+            System.ConsoleKey userInput = Console.ReadKey(true).Key;
+
+            switch (userInput) {
+                case ConsoleKey.UpArrow: myCar.GoUp(); break;
+                case ConsoleKey.DownArrow: myCar.GoDown(); break;
+                case ConsoleKey.LeftArrow: myCar.GoLeft(); break;
+                case ConsoleKey.RightArrow: myCar.GoRight(); break;
+                case ConsoleKey.R: myCar.RunEngine(); break;
+                case ConsoleKey.E: break;
+            }
+        }
+
+    }
+}
