e4c4caaebaa29cbe7fa378035a323a8170824c3f
Radosław Smoroński <43452468+Johanez07@users.noreply.github.com>
2023-06-13 16:50:23 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,57 @@
-﻿using CarMD;
+using CarMD;
 using System.Runtime.ConstrainedExecution;
 using System.Windows.Input;
+using System.Threading;
+using static Program;
+
 
 internal class Program
 {
+    //public char[,] pixels = new char[60, 5];
+
     private static void Main(string[] args)
     {
-        
-        Car myCar = new Car();
-        DisplayCar screen = new DisplayCar();
+        int screenX = 50;
+        int screenY = 15;
 
-        while(true) {
+        char[,] pixels = new char[screenX, screenY];
+
+
+        Obj obj = new Obj(pixels, screenX, screenY, 0, "cat", 1, 1);
+
+
+        //Car myCar = new Car();
+        DisplayCar screen = new DisplayCar(screenX, screenY);
+
+        while(true)
+        {
+            Console.Clear();
+            Console.WriteLine("CarMD v. 0.2 - 13/06/2023");
+            Console.WriteLine("Screen Size: " + screenX + ", " + screenY);
+            Console.WriteLine("TestObj: " +obj.x+", "+obj.y+"   " + obj.name);
+            screen.ShowScreen(pixels);
+
+
+            System.ConsoleKey userInput = Console.ReadKey(true).Key;
+
+            switch (userInput)
+            {
+                case ConsoleKey.UpArrow: obj.Move(pixels, 0, -1); break;
+                case ConsoleKey.DownArrow: obj.Move(pixels, 0, 1); break;
+                case ConsoleKey.LeftArrow: obj.Move(pixels, -1, 0); break;
+                case ConsoleKey.RightArrow: obj.Move(pixels, 1, 0); break;
+                case ConsoleKey.E: break;
+            }
+
+
+        }
+
+
+
+
+
+
+        /*while(true) {
             Console.Clear();
 
             screen.Display(myCar);
@@ -25,7 +66,7 @@ internal class Program
                 case ConsoleKey.R: myCar.RunEngine(); break;
                 case ConsoleKey.E: break;
             }
-        }
+        }*/
 
     }
 }
